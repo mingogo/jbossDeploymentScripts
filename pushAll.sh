3c766b54342add1bc53529355cfb279cada7f924
mingogo <mingogo.teng@gmail.com>
2014-10-13 11:47:53 -0400

    add things.

--- pushAll.sh
+++ pushAll.sh
@@ -3,6 +3,8 @@ git status
 git add -A
 git commit -m "add things."
 echo "[GIT] Which branch you want to push to remote?"
+git fetch
+git branch -a
 read BRANCH
 git push origin $BRANCH
 echo "[GIT] Done."
