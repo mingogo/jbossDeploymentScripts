040a8917aea67736295b28f9113c58d0eb163c71
mteng <mingogo.teng@gmail.com>
2014-10-13 11:50:47 -0400

    add things.

--- pushAll.sh
+++ pushAll.sh
@@ -5,7 +5,7 @@ git commit -m "add things."
 git fetch
 git branch -a
 echo "[GIT] Which branch do you want to push to remote?"
-echo "Enter:" 
 read BRANCH
+echo "[GIT] Push $BRANCH to Github."
 git push origin $BRANCH
 echo "[GIT] Done."
